7d40bcd75e9f7471d5fb17d8eb050dd0f798e319
NovelistChan <774527381@qq.com>
2021-06-02 19:36:14 +0800

    testcase

--- src/main/java/info/nemoworks/udo/poc/PocController.java
+++ src/main/java/info/nemoworks/udo/poc/PocController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PocController {
     @GetMapping(value = "/")
     public String hello() {
-        return "world";
+        return "{name: testName}";
     }
 
 }
